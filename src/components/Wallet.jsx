df531b7fc4ac9ff84217c296401175b27fde32bc
Felipe Shcolnik <shcolnik@gmail.com.br>
2022-05-10 12:15:36 -0300

    title tab

---
+++ src/components/Wallet.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Wallet extends React.Component {
+  render() {
+    return ( 
+      <div className="wallet-container">
+        <h1>W)allet Project</h1>
+      </div>
+    );
+  }
+}
+
+export default Wallet;
